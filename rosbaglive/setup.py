84c310d00faa430fd3fd497081d6d3f010524412
David Lu!! <davidvlu@gmail.com>
2013-07-31 23:04:21 -0500

    Correct python releasing

--- rosbaglive/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup()
-d['packages'] = []
-d['scripts'] = ['src/play.py']
-d['package_dir'] = {}
-
-setup(**d)
