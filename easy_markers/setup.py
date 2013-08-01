597505873e35afc04843164d67033e2d35a65a3d
David Lu!! <davidvlu@gmail.com>
2013-07-31 22:58:14 -0500

    Correct python releasing

--- easy_markers/setup.py
+++ easy_markers/setup.py
@@ -1,11 +1,10 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-package_info = genderate_distutils_setup(
+package_info = generate_distutils_setup(
     packages=['easy_markers'],
-    package_dir={'easy_markers': 'src'},
-    requires=['rospy'])
+    package_dir={'': 'src'},
 )
 
-setup(**setup_args)
+setup(**package_info)
 
